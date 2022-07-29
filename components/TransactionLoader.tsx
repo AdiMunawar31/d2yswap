5afd37576453fc5f5fa35d3c3a2dafc7ce03de53
Adi Munawar <munawar.adi31@gmail.com>
2022-07-29 23:29:57 +0700

    initial commit for d2yswap

---
+++ components/TransactionLoader.tsx
@@ -0,0 +1,17 @@
+import { MoonLoader } from 'react-spinners';
+
+const style = {
+	wrapper: `text-white h-96 w-72 flex flex-col justify-center items-center`,
+	title: `font-semibold text-xl mb-12`,
+};
+
+const TransactionLoader = () => {
+	return (
+		<div className={style.wrapper}>
+			<div className={style.title}>Transaction in progress...</div>
+			<MoonLoader color={'#fff'} loading={true} className='block mx-auto border-white' size={50} />
+		</div>
+	);
+};
+
+export default TransactionLoader;
